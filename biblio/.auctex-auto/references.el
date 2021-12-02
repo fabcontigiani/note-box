99c721ef86f70d9374bf019cf8a837753c56e8d5
Fabrizio Contigiani <fabcontigiani@gmail.com>
2021-12-01 22:57:48 -0300

    Misc

---
+++ biblio/.auctex-auto/references.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "silberschatz18_operat"
+    "gabbrielli10_progr"
+    "roy04_concep"
+    "roy12_progr"
+    "phillips10_python"
+    "serway18_fisica2"
+    "chang13_quimica"
+    "hopcroft08_automatas"
+    "larson16_calculo1"))
+ :bibtex)
+
